3eecb2ae5af55369427653d2231e60f44b64b9ab
dacemage <nick.dace@gmail.com>
2015-02-17 00:03:12 -0500

    Camera movement
    Actual working version of the camera controller.  Camera moves and
    rotates and dodges.  Camera shakes when attacking.

--- Slapper/Assets/Scripts/CameraControl.cs
+++ Slapper/Assets/Scripts/CameraControl.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour {
 
-	public float smooth = 3.0f;
+	public float smooth = 5.0f;
+	public int rotAngle = 10;
+	public float posMove = 0.2f;
 	private GameObject player;
 	private Transform playerT;
 	private Vector3 relCameraPos;
@@ -11,6 +13,7 @@ public class CameraControl : MonoBehaviour {
 	private Vector3 camMove;
 	private Vector3 startPos;
 	private Quaternion cameraRot;
+	private Quaternion startRot;
 	private Quaternion rotate;
 	private Quaternion lRotate;
 	private Quaternion rRotate;
@@ -21,20 +24,20 @@ public class CameraControl : MonoBehaviour {
 		playerT = player.transform;
 		cameraPos = this.transform.position;
 		startPos = this.transform.position;
+		startRot = this.transform.rotation;
 		cameraRot = this.transform.rotation;
 		relCameraPos = transform.position - playerT.position;
-		camMove = new Vector3(0.2f,0.0f,0.0f);
-		lRotate =  Quaternion.Euler(0,10,0)*cameraRot;
-		rRotate = Quaternion.Euler(0,-10,0)*cameraRot;
+		camMove = new Vector3(posMove,0.0f,0.0f);
+		lRotate =  Quaternion.Euler(0,rotAngle,0)*cameraRot;
+		rRotate = Quaternion.Euler(0,-rotAngle,0)*cameraRot;
 	}
 
 	void FixedUpdate(){
 		Vector3 standardPos = playerT.position + relCameraPos;
-		Quaternion standardRot = cameraRot;
-		Vector3 leftPos = cameraPos - camMove;//playerT.position + Vector3.left * relCameraPosMag;
-		Vector3 rightPos = cameraPos + camMove;//playerT.position + Vector3.right * relCameraPosMag;
+		Vector3 leftPos = cameraPos - camMove;
+		Vector3 rightPos = cameraPos + camMove;
 		status = player.GetComponent<PlayerStatus>();
-
+		
 		if(status.dodgeLeft==true){
 			transform.position = Vector3.Lerp(transform.position, leftPos, smooth * Time.deltaTime);
 			transform.rotation = Quaternion.Slerp(transform.rotation, lRotate, smooth * Time.deltaTime);
@@ -44,9 +47,9 @@ public class CameraControl : MonoBehaviour {
 			transform.rotation = Quaternion.Slerp(transform.rotation, rRotate, smooth * Time.deltaTime);
 		}
 		if(status.dodgeLeft==false&&status.dodgeRight==false){
-			transform.position = standardPos;
-			//transform.position = startPos;
-			transform.rotation = standardRot;
+			transform.position = Vector3.Lerp(transform.position, standardPos, smooth * Time.deltaTime);
+			//transform.position = Vector3.Lerp(transform.position, startPos, smooth * Time.deltaTime);
+			transform.rotation = Quaternion.Slerp(transform.rotation, startRot, smooth * Time.deltaTime);
 		}
 	}
 }

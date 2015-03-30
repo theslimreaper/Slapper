64262ee6bee5e539a1e03c2154f63874adebf810
Joel Vann <jvann@ltu.edu>
2015-03-30 18:52:49 -0400

    fixed lightShifter so the spotlight flash at the intended speed instead of at each frame.

--- Slapper/Assets/Scripts/LightShifter.cs
+++ Slapper/Assets/Scripts/LightShifter.cs
@@ -10,26 +10,30 @@ public class LightShifter : MonoBehaviour {
 	float currentTime;
 	public int minSpeed=-2;
 	public int maxSpeed=2;
-	static float maxHitFlickerTime=.5f;
-	static float hitTimer=0f;
-	public float flickerDuration=.5f;
-	float flickerTimer=0f;
 	public Color color0 = Color.red;
 	public Color color1 = Color.blue;
 	static bool enraged=false;//when the boss is made speed it up
 	float temp=0;
+
+
+	public int totalFlickers=4;//amount of times the light will flicker
+	static int currentFlicker=5;
+	public float maxFlickerTime=.05f;//amount of time a flicker lasts
+	static float currentFlickerTime;
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		currentTime = directionChangeTimer;
 		enraged = false;
 		PointLight.color = color1;
-
+		currentFlickerTime = maxFlickerTime;
 	}
 	
-	// Update is called once per frame
-	void FixedUpdate () {
+	//fixed update is called at a set delta time
+	void FixedUpdate () 
+	{
 		currentTime-=Time.deltaTime;
-		if(currentTime<=0)
+		if(currentTime<=0)//change the direction the spotlights are moving 
 		{
 			x1=Random.Range (minSpeed,maxSpeed);
 			x2=Random.Range (minSpeed,maxSpeed);
@@ -39,46 +43,43 @@ public class LightShifter : MonoBehaviour {
 			z2=Random.Range (minSpeed,maxSpeed);
 			currentTime=directionChangeTimer;
 		}
-		spotLight1.transform.Rotate (x1*Time.timeScale, y1*Time.timeScale, z1*Time.timeScale);
+		spotLight1.transform.Rotate (x1*Time.timeScale, y1*Time.timeScale, z1*Time.timeScale);//move the spotlights
 		spotLight2.transform.Rotate (x2*Time.timeScale, y2*Time.timeScale, z2*Time.timeScale);
-		if(hitTimer>0)
-		{
-			hitTimer -= Time.deltaTime;
-			flickerTimer-=Time.deltaTime;
-			if(flickerTimer<0)
-			{
 
-				if(spotLight1.enabled==true)
-				{
-				//	print ("turn off the lights");
-					spotLight1.enabled=false;
-					spotLight2.enabled=false;
-				}
-				else if(spotLight1.enabled==false)
-				{
-				//	print ("turn on the lights");
-					spotLight1.enabled=true;
-					spotLight2.enabled=true;
-				}
-				else 
-					flickerTimer=flickerDuration;
-			}
-		}
-		else
-		{
-			spotLight1.enabled=true;
-			spotLight2.enabled=true;
-		}
-		if(enraged==true)
+		if(enraged==true)//lerp the color when hes enraged
 		{
 			PointLight.color=Color.Lerp(Color.blue, Color.red,temp);
 			temp+=2*Time.deltaTime;
 		}
 	}
+	// Update is called once per frame
+	void Update()
+	{
+		if(currentFlicker<=totalFlickers)//flickers the lights when the enemy is hit
+		{
+			if(currentFlickerTime<=maxFlickerTime)
+				currentFlickerTime+=Time.deltaTime;
+			else
+			{
+				currentFlickerTime=0;
+				if(!spotLight1.enabled)
+				{
+				currentFlicker++;
+				spotLight1.enabled=true;
+				spotLight2.enabled=true;
+				}
+				else
+				{
+					spotLight1.enabled=false;
+					spotLight2.enabled=false;
+				}
+			}
+		}
+	}
 
-	public static void hit(float percent) {
-	//	print (percent + " % ");
-		hitTimer = maxHitFlickerTime;
+	public static void hit(float percent) 
+	{
+		currentFlicker = 0;
 		if(percent<.35f)
 			enraged=true;
 	}
